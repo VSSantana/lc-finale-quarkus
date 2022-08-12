8eb52b4b3cc87c1427bbb53148b8fb1beea24dfe
Vinícius de Sousa Santana <sousavinicius@hotmail.com>
2022-08-12 11:44:25 -0300

    Adicionada a feature de criar cliente

--- src/main/java/br/com/letscode/rest/ClienteResource.java
+++ src/main/java/br/com/letscode/rest/ClienteResource.java
@@ -7,38 +7,43 @@ import java.util.Optional;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import org.apache.http.client.utils.URIBuilder;
 
 import br.com.letscode.dto.ClientDto;
+import br.com.letscode.form.ClientForm;
 import br.com.letscode.model.Client;
-import br.com.letscode.repository.ClientRepository;
+import br.com.letscode.service.ClientService;
 
 @Path("/client")
 public class ClienteResource {
 
     @Inject
-    private ClientRepository clientRepository;
+    private ClientService clientService;
 
     @GET
     @Path("/list")
     public List<ClientDto> getClients() {
-        List<Client> clients = clientRepository.listAll();
+        List<Client> clients = clientService.geClienttList();
         return ClientDto.convertion(clients);
     }
 
-    // @POST
-    // @Transactional
-    // public ResponseEntity<ClientDto> insertClient(@RequestBody @Valid ClientForm
-    // client,
-    // UriComponentsBuilder uriBuilder) {
-    // Client newClient = client.convertion();
-    // clientRepository.save(newClient);
-    // URI uri =
-    // uriBuilder.path("/client/details/{id}").buildAndExpand(newClient.getId()).toUri();
-    // return ResponseEntity.created(uri).body(new ClientDto(newClient));
-    // }
+    @POST
+    public Response insertClient(@Valid ClientForm client) {
+        Client newClient = client.convertion();
+        clientService.insertClient(newClient);
+        URI uri = UriBuilder.fromPath("/client/details/{id}").build(newClient.getId());
+        return Response.created(uri).build();
+    }
 
     // @RequestMapping("/details/{id}")
     // @GetMapping
--- src/main/java/br/com/letscode/service/ClientService.java
+++ src/main/java/br/com/letscode/service/ClientService.java
@@ -2,20 +2,38 @@ package br.com.letscode.service;
 
 import java.util.List;
 
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import br.com.letscode.model.Client;
+import br.com.letscode.repository.ClientRepository;
 
 @Path("/client/list")
 @RegisterRestClient(configKey = "quarkus-api")
-public interface ClientService {
+public class ClientService {
+
+    @Inject
+    ClientRepository clientRepository;
 
     @GET
-    @javax.ws.rs.Produces(MediaType.APPLICATION_JSON)
-    public List<Client> getList();
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Client> geClienttList() {
+        return clientRepository.listAll();
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void insertClient(Client client) {
+        clientRepository.persist(client);
+    }
 
 }
