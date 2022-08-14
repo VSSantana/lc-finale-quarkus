a16260969fff909bad643d865ecd98c0b8c79edb
Vinícius de Sousa Santana <sousavinicius@hotmail.com>
2022-08-14 15:08:11 -0300

    Inclusão do Swagger :)

--- src/main/java/br/com/letscode/rest/CategoryRessource.java
+++ src/main/java/br/com/letscode/rest/CategoryRessource.java
@@ -17,6 +17,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import br.com.letscode.dto.CategoryDto;
 import br.com.letscode.form.CategoryForm;
 import br.com.letscode.model.Category;
@@ -31,6 +36,10 @@ public class CategoryRessource {
     @GET
     @Path("/list")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Recupera a lista de categorias da base de dados", description = "Lista de categorias no formato JSON")
+    @APIResponse(responseCode = "200", description = "Recupera lista de categorias", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Category.class))
+    })
     public List<CategoryDto> getCategories() {
         List<Category> categories = categoryService.geCategoryList();
         return CategoryDto.convertion(categories);
@@ -39,6 +48,10 @@ public class CategoryRessource {
     @GET
     @Path("/details/{id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Recupera os detalhes de uma categoria da base de dados", description = "Apresenta o registro de uma categoria no formato JSON")
+    @APIResponse(responseCode = "200", description = "Recupera o registro de uma categoria", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Category.class))
+    })
     public Response getCategory(@PathParam("id") Long id) {
         Optional<Category> category = categoryService.getCategory(id);
         if (category.isPresent()) {
@@ -50,6 +63,10 @@ public class CategoryRessource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Insere uma categoria na base de dados", description = "Inclui uma categoria na base de dados")
+    @APIResponse(responseCode = "201", description = "Inserir categoria", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Category.class))
+    })
     public Response insertCategory(@Valid CategoryForm categoryForm) {
         Category newCategory = categoryForm.convertion();
         categoryService.insertCategory(newCategory);
@@ -61,6 +78,10 @@ public class CategoryRessource {
     @Transactional
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Altera uma categoria na base de dados", description = "Altera uma categoria na base de dados")
+    @APIResponse(responseCode = "200", description = "Alterar categoria", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Category.class))
+    })
     public Response alterCategory(@PathParam("id") Long id, @Valid CategoryForm categoryForm) {
         Category category = categoryService.alterCategory(id, categoryForm.convertion());
         if (category != null) {
@@ -72,6 +93,10 @@ public class CategoryRessource {
     @DELETE
     @Path("/{id}")
     @Transactional
+    @Operation(summary = "Deleta uma categoria da base de dados", description = "Deleta uma categoria da base de dados")
+    @APIResponse(responseCode = "200", description = "Deletar categoria", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Category.class))
+    })
     public Response deleteCategory(@PathParam("id") Long id) {
         Category category = categoryService.deleteCategory(id);
         if (category != null) {
--- src/main/java/br/com/letscode/rest/ClienteResource.java
+++ src/main/java/br/com/letscode/rest/ClienteResource.java
@@ -17,6 +17,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+
 import br.com.letscode.dto.ClientDto;
 import br.com.letscode.form.ClientForm;
 import br.com.letscode.model.Client;
@@ -31,6 +36,10 @@ public class ClienteResource {
     @GET
     @Path("/list")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Recupera a lista de clientes da base de dados", description = "Lista de clientes no formato JSON")
+    @APIResponse(responseCode = "200", description = "Recupera lista de clientes", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Client.class))
+    })
     public List<ClientDto> getClients() {
         List<Client> clients = clientService.geClientList();
         return ClientDto.convertion(clients);
@@ -39,6 +48,10 @@ public class ClienteResource {
     @GET
     @Path("/details/{id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Recupera os detalhes de um cliente da base de dados", description = "Apresenta o registro de um cliente no formato JSON")
+    @APIResponse(responseCode = "200", description = "Recupera o registro de um cliente", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Client.class))
+    })
     public Response getClient(@PathParam("id") Long id) {
         Optional<Client> client = clientService.getClient(id);
         if (client.isPresent()) {
@@ -50,6 +63,10 @@ public class ClienteResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Insere um cliente na base de dados", description = "Inclui um cliente na base de dados")
+    @APIResponse(responseCode = "201", description = "Inserir cliente", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Client.class))
+    })
     public Response insertClient(@Valid ClientForm clientForm) {
         Client newClient = clientForm.convertion();
         Client createdClient = clientService.insertClient(newClient);
@@ -64,6 +81,10 @@ public class ClienteResource {
     @Transactional
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Altera um cliente na base de dados", description = "Altera um cliente na base de dados")
+    @APIResponse(responseCode = "200", description = "Alterar cliente", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Client.class))
+    })
     public Response alterClient(@PathParam("id") Long id, @Valid ClientForm clientForm) {
         Client client = clientService.alterClient(id, clientForm.convertion());
         if (client != null) {
@@ -75,6 +96,10 @@ public class ClienteResource {
     @DELETE
     @Path("/{id}")
     @Transactional
+    @Operation(summary = "Deleta um cliente da base de dados", description = "Deleta um cliente da base de dados")
+    @APIResponse(responseCode = "200", description = "Deletar cliente", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Client.class))
+    })
     public Response deleteClient(@PathParam("id") Long id) {
         Client client = clientService.deleteClient(id);
         if (client != null) {
